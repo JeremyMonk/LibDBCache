d9dbf5f66aff7f4fb33647e16f42ee35fe8e95e5
Jeremy G <jjwpzzx@gmail.com>
2024-07-16 07:25:47 -0500

    initialize delay_auto_attack in find_spell

--- main.lua
+++ main.lua
@@ -126,6 +126,7 @@ function LibDBCache:find_spell( spellID, rank )
     spell.initial_stacks    = spell.initial_stacks or spell.max_stacks
     spell.trigger_rate      = spell.trigger_rate or 1
     
+    spell.delay_auto_attack = spell.delay_auto_attack or false
     spell.frontal           = spell.frontal or false
     spell.channeled         = spell.channeled or false
     spell.duration_hasted   = spell.duration_hasted or false
