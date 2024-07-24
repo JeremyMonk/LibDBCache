924a9f4772ba32105bea62c8e073888d7d09626e
Jeremy G <jjwpzzx@gmail.com>
2024-07-24 12:21:49 -0500

    Rebuild Spelldata

--- database/db_100207_55261.lua
+++ database/db_100207_55261.lua
@@ -1,5 +1,5 @@
 -- ------------------------------------------------------------------------------
--- Database generated from DBCache on 2024-07-16 - World of Warcraft 10.2.7.55261
+-- Database generated from DBCache on 2024-07-23 - World of Warcraft 10.2.7.55261
 -- ------------------------------------------------------------------------------
 
 if not validVersion( "10.2.7", 55261 ) then 
--- main.lua
+++ main.lua
@@ -18,7 +18,7 @@ local tonumber = tonumber
 local GetTime = GetTime
 
 -- Versioning
-LibDBCache.Version = 2.5
+LibDBCache.Version = 2.6
 
 -- ------------------------------------------------------------------------------
 
